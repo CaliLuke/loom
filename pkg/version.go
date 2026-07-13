cf367981d2bfde307e0b975df95d6e33bab5d3e6
Luca Candela <luca.candela@gmail.com>
2026-07-12 20:42:56 -0700

    Release v1.6.0

--- pkg/version.go
+++ pkg/version.go
@@ -10,9 +10,9 @@ const (
 	// Major version number
 	Major = 1
 	// Minor version number
-	Minor = 5
+	Minor = 6
 	// Build number
-	Build = 1
+	Build = 0
 	// Suffix - set to empty string in release tag commits.
 	Suffix = ""
 )
