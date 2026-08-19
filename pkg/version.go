d056271f24a9e2e530f1f2e43a9bf76185b185d6
Luca Candela <luca.candela@gmail.com>
2026-08-18 22:16:43 -0700

    Release v1.8.0-alpha.20

--- pkg/version.go
+++ pkg/version.go
@@ -14,7 +14,7 @@ const (
 	// Build number
 	Build = 0
 	// Suffix is the semantic version prerelease identifier, or empty for a stable release.
-	Suffix = "alpha.19"
+	Suffix = "alpha.20"
 )
 
 var (
